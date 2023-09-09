113c89cfaca5cfd83ac865fd121969c88995f29a
eletroniclab <143656433+eletroniclab@users.noreply.github.com>
2023-09-09 11:35:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("btnTop")
+
+btn.addEventListener("click", function(){
+    window.scrollTo(0,0)
+})
+
+document.addEventListener('scroll' ,ocultar)
+
+function ocultar(){
+    if(window.scrollY > 100){
+        btn.style.display = "flex"
+    }else{
+        btn.style.display = "none"
+    }
+}
+
+ocultar()
